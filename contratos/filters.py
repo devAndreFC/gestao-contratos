0ce5f1b3725d0e9bb9eff1c18c46828b1317fe2a
Andre Oliveira <gbdev06@gbringel.com>
2025-06-08 22:09:26 -0400

    feat: adicionado filtros dos contratos.

---
+++ contratos/filters.py
@@ -0,0 +1,32 @@
+import django_filters
+from datetime import datetime
+from contratos.models import Contrato
+
+
+class ContratosFilter(django_filters.FilterSet):
+    id = django_filters.NumberFilter(field_name="id", lookup_expr="exact")
+    cpf = django_filters.CharFilter(field_name="numero_documento", lookup_expr="exact")
+    estado = django_filters.CharFilter(field_name="estado", lookup_expr="iexact")
+
+    data = django_filters.CharFilter(method="filter_data")
+
+    class Meta:
+        model = Contrato
+        fields = ["id", "cpf", "data", "estado"]
+
+    def filter_data(self, queryset, name, value):
+        try:
+            data = datetime.strptime(value, "%m/%Y")
+            return queryset.filter(
+                data_emissao__year=data.year,
+                data_emissao__month=data.month,
+            )
+        except ValueError:
+            pass
+
+        try:
+            data = datetime.strptime(value, "%Y")
+            return queryset.filter(data_emissao__year=data.year)
+        except ValueError:
+            pass
+        return queryset
--- contratos/views/contrato.py
+++ contratos/views/contrato.py
@@ -1,11 +1,14 @@
 from rest_framework import viewsets
 from contratos.models import Contrato
 from contratos.serializers import ContratoSerializer, ContratoListSerializer
-
+from django_filters.rest_framework import DjangoFilterBackend
+from contratos.filters import ContratosFilter
 
 class ContratoViewSet(viewsets.ModelViewSet):
-    queryset = Contrato.objects.all("-id")
+    queryset = Contrato.objects.all().order_by("-id")
     serializer_class = ContratoSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = ContratosFilter
 
     def get_serializer_class(self):
         if self.action == "list":
